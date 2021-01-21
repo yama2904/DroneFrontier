5c13f1ff1c99dc14abbd74fb10b0120ea26a0258
yamamoto <tenfive1227@gmail.com>
2021-01-21 11:46:23 +0900

    レースオブジェクトの追加 DroneGuardの修正中

---
+++ DroneFrontier/Assets/MainGame/Race/Drone_Guard/DroneGuard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneGuard : MonoBehaviour
+{
+    enum Pattern
+    {
+        ONE,
+        TWO,
+        THREE,
+
+        NONE
+    }
+
+    [SerializeField] Pattern pattern = Pattern.NONE;
+
+    float nowPosi;
+
+    void Start()
+    {
+        nowPosi = this.transform.position.y;
+    }
+
+    void Update()
+    {
+        if (pattern == Pattern.ONE)
+        {
+            transform.position = new Vector3(transform.position.x, nowPosi + Mathf.PingPong(Time.time * 10, 7.5f), transform.position.z);
+        }
+        if(pattern == Pattern.TWO)
+        {
+            transform.position = new Vector3(transform.position.x, nowPosi + Mathf.PingPong(Time.time * 15, 9.5f), transform.position.z);
+        }
+        if(pattern == Pattern.THREE)
+        {
+            transform.position = new Vector3(transform.position.x, nowPosi + Mathf.PingPong(Time.time * 5, 5.5f), transform.position.z);
+        }
+    }
+}
---
+++ DroneFrontier/Assets/MainGame/Race/itemBoxScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemBoxScript : MonoBehaviour
+{
+    float nowPosi;
+
+    void Start()
+    {
+        nowPosi = this.transform.position.y;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, nowPosi + Mathf.PingPong(Time.time * 10, 7.5f), transform.position.z);
+    }
+}
