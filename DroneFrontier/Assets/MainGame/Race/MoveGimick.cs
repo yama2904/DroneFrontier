ca4dd86ae9fe7a77753aae614fb9fcb27705e8b5
yamamoto <tenfive1227@gmail.com>
2021-01-27 10:46:11 +0900

    a

--- DroneFrontier/Assets/MainGame/Race/MoveGimick.cs
+++ DroneFrontier/Assets/MainGame/Race/MoveGimick.cs
@@ -4,37 +4,39 @@ using UnityEngine;
 
 public class MoveGimick : MonoBehaviour
 {
-    enum Type
+    enum Dir
     {
-        ONE,
-        TWO,
-        THREE,
+        DIR_X,
+        DIR_Y,
+        DIR_Z,
 
         NONE
     }
-    [SerializeField] Type type = Type.NONE;
+    [SerializeField] Dir type = Dir.NONE;
+    [SerializeField] float speed = 1f;
+    [SerializeField, Tooltip("移動距離")] float range = 7.5f;
     Transform cacheTransform = null;
-    float nowPosi = 0;
+    Vector3 initPos;
 
     void Start()
     {
         cacheTransform = transform;
-        nowPosi = cacheTransform.position.y;
+        initPos = cacheTransform.position;
     }
     
     void Update()
     {
-        if(type == Type.ONE)
+        if(type == Dir.DIR_X)
         {
-            cacheTransform.position = new Vector3(nowPosi + Mathf.PingPong(Time.time * 7, 7.5f), cacheTransform.position.y, cacheTransform.position.z);
+            cacheTransform.position = new Vector3(initPos.x + Mathf.PingPong(Time.time * speed, range), initPos.y, initPos.z);
         }
-        if(type == Type.TWO)
+        if(type == Dir.DIR_Y)
         {
-            cacheTransform.position = new Vector3(nowPosi + Mathf.PingPong(Time.time * 7, 30f), cacheTransform.position.y, cacheTransform.position.z);
+            cacheTransform.position = new Vector3(initPos.x, initPos.y + Mathf.PingPong(Time.time * speed, range), initPos.z);
         }
-        if(type == Type.THREE)
+        if(type == Dir.DIR_Z)
         {
-            cacheTransform.position = new Vector3(cacheTransform.position.x, nowPosi + Mathf.PingPong(Time.time * 10, 7.5f), cacheTransform.position.z);
+            cacheTransform.position = new Vector3(initPos.x, initPos.y, initPos.z + Mathf.PingPong(Time.time * speed, range));
         }
     }
 }
