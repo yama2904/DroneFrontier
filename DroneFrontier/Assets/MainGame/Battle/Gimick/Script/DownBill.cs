1909d87a571888896e46125e25dc82df77afdcf0
yamamoto <tenfive1227@gmail.com>
2021-01-30 21:13:13 +0900

    DownBill系スクリプトの修正
    DownHighBill・DownMidBill・DownLowBillスクリプトを統合し、インスペクターを見やすくしたDownBillスクリプトの作成
    それに伴いDownHighBillスクリプト、DownMidBillスクリプト、DownLowBillスクリプトを削除

--- DroneFrontier/Assets/MainGame/Battle/Gimick/Script/DownBill.cs
+++ DroneFrontier/Assets/MainGame/Battle/Gimick/Script/DownBill.cs
@@ -2,20 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DownHighBill : MonoBehaviour
+public class DownBill : MonoBehaviour
 {
-    [SerializeField, Tooltip("1回目の沈下速度")] float firstDownSpeed = 10f;
-    [SerializeField, Tooltip("2回目の沈下速度")] float secondDownSpeed = 10f;
-    [SerializeField, Tooltip("3回目の沈下速度")] float thirdDownSpeed = 10f;
+    enum DownNum
+    {
+        ONE,
+        TWO,
+        THREE,
 
-    [SerializeField, Tooltip("1回目の沈下開始時間")] float firstDownTime = 60f;
-    [SerializeField, Tooltip("2回目の沈下開始時間")] float secondDownTime = 150f;
-    [SerializeField, Tooltip("3回目の沈下開始時間")] float thirdDownTime = 240f;
+        NONE
+    }
+    [SerializeField, Tooltip("沈下する回数")] DownNum downNum = DownNum.NONE;
 
-    [SerializeField, Tooltip("1回目の沈下が終了するY座標")] float firsDownPos = 0;
-    [SerializeField, Tooltip("2回目の沈下が終了するY座標")] float secondDownPos = 0;
-    [SerializeField, Tooltip("3回目の沈下が終了するY座標")] float thirdDownPos = 0;
+    [Header("1回目")]
+    [SerializeField, Tooltip("沈下速度")] float firstDownSpeed = 10f;
+    [SerializeField, Tooltip("沈下開始時間")] float firstDownTime = 60f;
+    [SerializeField, Tooltip("沈下が終了するY座標")] float firstDownPos = -14f;
 
+    [Header("2回目")]
+    [SerializeField, Tooltip("沈下速度")] float secondDownSpeed = 10f;
+    [SerializeField, Tooltip("沈下開始時間")] float secondDownTime = 150f;
+    [SerializeField, Tooltip("沈下が終了するY座標")] float secondDownPos = -24f;
+
+    [Header("3回目")]
+    [SerializeField, Tooltip("沈下速度")] float thirdDownSpeed = 10f;
+    [SerializeField, Tooltip("沈下開始時間")] float thirdDownTime = 240f;
+    [SerializeField, Tooltip("沈下が終了するY座標")] float thirdDownPos = -45f;
+
+    [Header("触れるでない")]
     [SerializeField] GameObject billObject = null;
     [SerializeField] GameObject particles = null;
 
@@ -29,17 +43,30 @@ public class DownHighBill : MonoBehaviour
     {
         particles.SetActive(false);
         cacheTransform = billObject.transform;
-        Invoke(nameof(StartFirsDown), firstDownTime);
-        Invoke(nameof(StartSecondDown), secondDownTime);
-        Invoke(nameof(StartThirdDown), thirdDownTime);
-    }
 
+        if (downNum == DownNum.ONE)
+        {
+            Invoke(nameof(StartFirsDown), firstDownTime);
+        }
+        if (downNum == DownNum.TWO)
+        {
+            Invoke(nameof(StartFirsDown), firstDownTime);
+            Invoke(nameof(StartSecondDown), secondDownTime);
+        }
+        if (downNum == DownNum.THREE)
+        {
+            Invoke(nameof(StartFirsDown), firstDownTime);
+            Invoke(nameof(StartSecondDown), secondDownTime);
+            Invoke(nameof(StartThirdDown), thirdDownTime);
+        }
+    }
+    
     void Update()
     {
         if (isFirst)
         {
             cacheTransform.Translate(0, firstDownSpeed * Time.deltaTime * -1, 0);
-            if (cacheTransform.localPosition.y < firsDownPos)
+            if (cacheTransform.localPosition.y < firstDownPos)
             {
                 isFirst = false;
                 if (!isSecond)
