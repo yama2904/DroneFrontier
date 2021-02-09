46a9fb140aa241bd87cd66290820694eacb1b97c
yamamoto <tenfive1227@gmail.com>
2021-02-09 10:26:56 +0900

    a

--- DroneFrontier/Assets/MainGame/Battle/Script/Online/BattleManager.cs
+++ DroneFrontier/Assets/MainGame/Battle/Script/Online/BattleManager.cs
@@ -282,6 +282,7 @@ namespace Online
                 FinishGame(ranking);
                 isFinished = true;
 
+                RpcSetTextEnabled(false);
                 StopCoroutine(countCoroutine);
             }
         }
