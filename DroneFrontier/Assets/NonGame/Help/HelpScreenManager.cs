6b90578b78b6ecaa203ae31a14b7ffc9a2a44d96
yamamoto <tenfive1227@gmail.com>
2021-02-10 14:25:13 +0900

    ヘルプ画面の作成

--- DroneFrontier/Assets/NonGame/Help/HelpScreenManager.cs
+++ DroneFrontier/Assets/NonGame/Help/HelpScreenManager.cs
@@ -1,25 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HelpScreenManager : MonoBehaviour
 {
+    [SerializeField] Image HelpBasicOperationImage = null;
+    [SerializeField] Image HelpBattleModeImage = null;
+    [SerializeField] Image HelpRaceModeImage = null;
+    enum Help
+    {
+        BASIC,
+        BATTLE,
+        RACE,
+
+        NONE
+    }
+    Help selectHelp = Help.NONE;
+
+    private void Start()
+    {
+        HelpBasicOperationImage.enabled = false;
+        HelpBattleModeImage.enabled = false;
+        HelpRaceModeImage.enabled = false;
+    }
+
+
     //基本操作
     public void SelectBasicOperation()
     {
-
+        HelpBasicOperationImage.enabled = true;
+        selectHelp = Help.BASIC;
     }
 
     //バトルモード
     public void SelectBattleModeHelp()
     {
-
+        HelpBattleModeImage.enabled = true;
+        selectHelp = Help.BATTLE;
     }
 
     //レースモード
     public void SelectRaceModeHelp()
     {
-
+        HelpRaceModeImage.enabled = true;
+        selectHelp = Help.RACE;
     }
 
     //戻る
@@ -28,6 +53,27 @@ public class HelpScreenManager : MonoBehaviour
         //SE再生
         SoundManager.Play(SoundManager.SE.CANCEL, SoundManager.BaseSEVolume);
 
-        BaseScreenManager.SetScreen(BaseScreenManager.Screen.GAME_MODE_SELECT);
+        if (selectHelp == Help.BASIC)
+        {
+            HelpBasicOperationImage.enabled = false;
+            selectHelp = Help.NONE;
+            return;
+        }
+        if (selectHelp == Help.BATTLE)
+        {
+            HelpBattleModeImage.enabled = false;
+            selectHelp = Help.NONE;
+            return;
+        }
+        if (selectHelp == Help.RACE)
+        {
+            HelpRaceModeImage.enabled = false;
+            selectHelp = Help.NONE;
+            return;
+        }
+        else
+        {
+            BaseScreenManager.SetScreen(BaseScreenManager.Screen.GAME_MODE_SELECT);
+        }
     }
 }
